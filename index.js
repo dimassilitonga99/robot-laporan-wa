8357e06ef8613d95b6627494ebcc52951830cea4
dimassilitonga99 <44350559+dimassilitonga99@users.noreply.github.com>
2026-05-26 11:19:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const GEMINI_KEY   = process.env.GEMINI_KEY;
 const FONNTE_TOKEN = process.env.FONNTE_TOKEN;
-const GEMINI_URL   = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_KEY}`;
+const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_KEY}`;
 
 async function kirimWA(target, message) {
   try {
